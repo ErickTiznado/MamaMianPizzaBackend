52f1539f34df383dfc719f363124dada4b5bf7e8
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-03-29 20:28:36 -0600

    Agrega la función createAdmin a userControllers para la creación de un usuario administrador.

--- contollers/userControllers.js
+++ contollers/userControllers.js
@@ -1,4 +1,6 @@
 const pool = require('../config/db');
+const bcrypt = require('bcrypt');
+
 
 exports.getAllUsers = (req, res) => {
     pool.query('SELECT * FROM users', (err, results) =>{
@@ -7,5 +9,26 @@ exports.getAllUsers = (req, res) => {
             return res.status(500).json({error: 'Error al obtener usuarios'})
         }
         res.json(results);
-    })
+    });
+};
+
+exports.createAdmin = (req, res) => {
+    try{
+    const {nombre, correo, contrasena, rol, telefono} = req.body;
+    
+        if(!nombre || !correo || !contrasena || !rol || !telefono){
+            return res.status(400).json({message: 'Faltan datos requeridos'});
+        }
+        const hashedPass = bcrypt.hashSync(contrasena, 10);
+        pool.query('INSERT INTO administradores ( nombre, correo, contrasena, rol, telefono) VALUES ( ?, ? ,? ,? ,?)', [ nombre, correo, hashedPass, rol, telefono], (err, results) => {
+            if(err){
+                console.error('Error al crear administrador', err);
+                return res.status(500).json({message: 'Error al crear el usuario administrador'})
+            }
+            res.status(201).json({message: 'Usuario Administrador creado exitosamente', id_admin: results.insertId});
+        });
+}catch(error){
+    console.error('Error en el servidor', error);
+    res.status(500).json({message: 'Error en el servidor'});
 }
+};
