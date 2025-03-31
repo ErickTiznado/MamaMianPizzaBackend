61af727e9f39ab92f3d294f1456a6d0bfffe03e4
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-03-30 19:52:27 -0600

    Agrega la ruta para la autenticación de administradores en userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../contollers/userControllers');
 
 router.get('/', userController.getAllUsers);
 router.post('/admins', userController.createAdmin);
-
+router.post('/login', userController.loginAdmin);
 
 
 module.exports = router;
