68e97bf1ed413c2082dd47bdb807c02afcfa80b3
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-04-05 19:42:15 -0600

    Corrige la ruta para la autenticación de usuarios en userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const userController = require('../contollers/userControllers');
 router.get('/', userController.getAllUsers);
 router.post('/admins', userController.createAdmin);
 router.post('/login', userController.loginAdmin);
-
+router.post('/users_login', userController.loginClient);
 
 module.exports = router;
