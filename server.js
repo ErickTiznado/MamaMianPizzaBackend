0ad2d2caf8377f8e6b1d257ac9741ce1d0673ad6
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2025-04-09 11:00:49 -0600

    Fix import path for database configuration in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const pool = require('./database/db');
+const pool = require('./config/db');
 
 // Import routes
 const userRoutes = require('./routes/userRoutes');
